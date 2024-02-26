e2077da7b156f3c871959e7847f694b156dcfc64
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-02-26 07:18:27 +0530

    Final Commit

---
+++ xstopwatch/src/App.jsx
@@ -0,0 +1,42 @@
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [isrunning, setisrunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+    if (isrunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [isrunning]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const remainingSeconds = time % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  const toggleHandler = () => {
+    setisrunning(!isrunning);
+  };
+
+  const reset = () => {
+    setisrunning(false);
+    setElapsedTime(0);
+  };
+  return (
+    <div>
+      <h1>StopWatch</h1>
+      <p>Time:{formatTime(elapsedTime)}</p>
+      <button onClick={toggleHandler}>{isrunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
